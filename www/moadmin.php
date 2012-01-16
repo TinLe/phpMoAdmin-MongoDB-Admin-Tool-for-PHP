c1a01988059e12e1b278e76a47b99466f644e7bf
Tin Le <tin@mashlogic.com>
2012-01-16 15:17:52 -0800

    Separate out config constants from code and put moadmin.php into web dir so we can better protect config constants.

--- www/moadmin.php
+++ www/moadmin.php
@@ -11,45 +11,7 @@
  * @license GPL v3 - http://vork.us/go/mvz5
  */
 
-/**
- * To enable password protection, uncomment below and then change the username => password
- * You can add as many users as needed, eg.: array('scott' => 'tiger', 'samantha' => 'goldfish', 'gene' => 'alpaca')
- */
-//$accessControl = array('scott' => 'tiger');
-
-/**
- * Uncomment to restrict databases-access to just the databases added to the array below
- * uncommenting will also remove the ability to create a new database
- */
-//moadminModel::$databaseWhitelist = array('admin');
-
-/**
- * Sets the design theme - themes options are: swanky-purse, trontastic and classic
- */
-define('THEME', 'trontastic');
-
-/**
- * To connect to a remote or authenticated Mongo instance, define the connection string in the MONGO_CONNECTION constant
- * mongodb://[username:password@]host1[:port1][,host2[:port2:],...]
- * If you do not know what this means then it is not relevant to your application and you can safely leave it as-is
- */
-define('MONGO_CONNECTION', '');
-
-/**
- * Set to true when connecting to a Mongo replicaSet
- * If you do not know what this means then it is not relevant to your application and you can safely leave it as-is
- */
-define('REPLICA_SET', false);
-
-/**
- * Default limit for number of objects to display per page - set to 0 for no limit
- */
-define('OBJECT_LIMIT', 100);
-
-/**
- * Contributing-developers of the phpMoAdmin project should set this to true, everyone else can leave this as false
- */
-define('DEBUG_MODE', false);
+require "../config.inc";
 
 /**
  * Vork core-functionality tools
@@ -2579,3 +2541,4 @@ mo.submitQuery = function() {
 echo '</div>'; //end of bodycontent
 
 echo $html->footer();
+?>
